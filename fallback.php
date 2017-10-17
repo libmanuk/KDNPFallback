166c704272578f15c6a1f0f8045c7310d30000c8
Eric C. Weig <eweig@email.uky.edu>
2017-10-17 14:05:50 -0400

    Update fallback.php

--- fallback.php
+++ fallback.php
@@ -7,7 +7,7 @@ function destination($id)
 
 function source($id)
 {
-    $trimmed_id = preg_replace('/-z$/', '', $id);
+    $trimmed_id = preg_replace('/-z|-t|-m$/', '', $id);
     $pt_path = preg_replace('/(..)/', '\1/', $trimmed_id);
     return "/home/libmanuk/public_html/pairtree_root/$pt_path$trimmed_id/$id.html";
 }
@@ -19,6 +19,38 @@ function uri($id, $terms)
 
 #                             1            2     3       4  5
 if (preg_match('/^\/?catalog\/([a-z0-9]{12}(-z)?)(.html)?(\?(.*))?$/', $_SERVER['REQUEST_URI'], $matches)) {
+    $wanted_id = $matches[1];
+    $search_terms = $matches[5];
+
+    copy(source($wanted_id), destination($wanted_id));
+
+    if (strlen($search_terms) > 0) {
+        header('Location: ' . uri($wanted_id, $search_terms));
+    }
+    else {
+        print(file_get_contents(destination($wanted_id)));
+    }
+    exit;
+}
+
+#                             1            2     3       4  5
+if (preg_match('/^\/?catalog\/([a-z0-9]{12}(-t)?)(.html)?(\?(.*))?$/', $_SERVER['REQUEST_URI'], $matches)) {
+    $wanted_id = $matches[1];
+    $search_terms = $matches[5];
+
+    copy(source($wanted_id), destination($wanted_id));
+
+    if (strlen($search_terms) > 0) {
+        header('Location: ' . uri($wanted_id, $search_terms));
+    }
+    else {
+        print(file_get_contents(destination($wanted_id)));
+    }
+    exit;
+}
+
+#                             1            2     3       4  5
+if (preg_match('/^\/?catalog\/([a-z0-9]{12}(-m)?)(.html)?(\?(.*))?$/', $_SERVER['REQUEST_URI'], $matches)) {
     $wanted_id = $matches[1];
     $search_terms = $matches[5];
 
